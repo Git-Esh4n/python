ace1f006d6b16af03f9f53f1ad940956baa49c29
github_eshan <eshankum2007@gmail.com>
2026-06-29 21:34:55 +0530

    Add files via upload

---
+++ chapter 2/operatos.py
@@ -0,0 +1,29 @@
+#Following are some common operators in python:
+# 1. Arithmetic operators: +, -, *, / etc.
+# 2. Assignment operators: =, +=, -= etc.
+# 3. Comparison operators: ==, >, >=, <, != etc.
+# 4. Logical operators: and, or, not.
+
+a = 12 
+print(a )
+
+b= 3 
+b+= 4 
+print(b)
+
+#comparison operators
+d = 5>4 
+
+print (d)
+
+x=  5 >=5 
+print (x )
+
+d= 5 != 5
+print (d)
+
+#logical operators 
+
+e = True  or False 
+print (e)
+
---
+++ chapter 2/variables.py
@@ -0,0 +1,7 @@
+a= 1 
+b=2 
+
+print(a+b)
+c= 4 
+
+name="eshan"
